3a73d3b55066f50a38426ef631d4b37010396353
janetranada <jyranada@gmail.com>
2019-12-19 15:44:36 +0200

    changed tweet button disabling from 'length=0' to 'if only spaces present'; added functionality to input new username

--- src/components/CreateTweet/index.js
+++ src/components/CreateTweet/index.js
@@ -15,39 +15,39 @@ class CreateTweet extends React.Component {
     }
 
     render() {
-        const { tweet} = this.state;
+        const { tweet } = this.state;
 
         return (
             <MyAppContext.Consumer>
-                {({tweets, addTweet, isPostingTweet}) => (
+                {({ tweets, addTweet, isPostingTweet }) => (
                     <div className="tweet-input-container">
-                        <textarea 
+                        <textarea
                             id="tweet-user-input"
                             className="tweet-text-area white-text"
                             placeholder="What do you have in mind.."
-                            onChange={(event) => this.handleTweetChange(event)}
+                            onChange={(event) => {this.handleTweetChange(event)}}
                             value={this.state.tweet}
                             disabled={isPostingTweet}
-                        >                    
+                        >
                         </textarea>
 
-                        {(tweet.length > 140) && 
+                        {(tweet.length > 140) &&
                             <div className="warning-length">
                                 The tweet can't contain more than 140 chars.
                             </div>
                         }
 
-                        <button 
+                        <button
                             className="white-text my-button"
                             onClick={() => {
                                 addTweet(tweet);
-                                this.setState({tweet: ''});
+                                this.setState({ tweet: '' });
                             }}
-                            disabled = {tweet.length > 140 || tweet.length === 0}
+                            disabled={tweet.length > 140 || tweet.trim() === ''}
                         >
                             Tweet
                         </button>
-                        
+
                     </div>
                 )}
             </MyAppContext.Consumer>
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-const User = () => {
-    return(
-        <div className="user-profile-container">
-            <p className="profile-header">Profile</p>
-            <p className="username-header">User Name</p>
-            <input className="username-input" placeholder="Eve"></input>
-            <button className="white-text my-button profile-page">Save</button>
-        </div>
-        
-    )
+class User extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: 'Eve'
+        }
+    }
+
+    render() {
+        const { user } = this.state;
+        return (
+            <div className="user-profile-container">
+                <p className="profile-header">Profile</p>
+                <p className="username-header">User Name</p>
+                <input 
+                    className="username-input" 
+                    placeholder="Please enter a username"
+                    value={user}
+                    onChange={event => this.setState({ user: event.target.value })}
+                />                   
+                
+                <button 
+                    className="white-text my-button profile-page"
+                    onClick={() => {}}
+                    disabled={user.length === 0}
+                >
+                    Save
+                </button>
+            </div>
+
+        )
+    }
+
 }
 
 export default User;
